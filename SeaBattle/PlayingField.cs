5c1ba49780fae32f30be37e62b7493edbe3e0f80
Настя Настя <8ipoet8@gmail.com>
2020-08-17 10:57:14 +0300

    add

--- SeaBattle/PlayingField.cs
+++ SeaBattle/PlayingField.cs
@@ -1,8 +1,33 @@
-﻿namespace SeaBattle
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace SeaBattle
 {
     class PlayingField
     {
         public int Width { get; set; }
         public int Height { get; set; }
+        public List<Ship> Ships { get; set; }
+
+        public void AddShip(Point startPoint, ShipType type)
+        {
+            Ship res;
+        
+        }
+
+        private void CheckEmptyPlace(Point startPoint, int lentgth)
+        {
+
+        }
+
+        public void SelectShip(int index)
+        {
+
+        }
+
+        public void GetAllShips()
+        {
+
+        }
     }
 }
