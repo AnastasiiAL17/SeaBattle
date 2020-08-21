32d020543fc813759b8a8701e55d5f17072356f5
Настя <ipoet@mail.ru>
2020-08-21 12:00:44 +0300

    delete empty variables

--- SeaBattle/Program.cs
+++ SeaBattle/Program.cs
@@ -7,13 +7,13 @@
         private static void Main(string[] args)
         {
             PlayingField playingField = new PlayingField();
-            _ = (AuxiliaryShip)playingField.AddShip(playingField.InitNewPoint(), ShipType.Auxiliary);
-            _ = (MilitaryShip)playingField.AddShip(playingField.InitNewPoint(), ShipType.Military);
+            playingField.AddShip(playingField.InitNewPoint(), ShipType.Auxiliary);
+            playingField.AddShip(playingField.InitNewPoint(), ShipType.Military);
             MixShip mixShip = (MixShip)playingField.AddShip(playingField.InitNewPoint(), ShipType.Mix);
             MixShip mixShip1 = (MixShip)playingField.AddShip(playingField.InitNewPoint(), ShipType.Mix);
             Console.WriteLine(playingField.GetAllShips());
             var t = mixShip.Equals(mixShip1);
-            var f = mixShip.Equals(mixShip1);
+            var f = mixShip.Equals(mixShip);
             var a = mixShip == mixShip1;
             var b = mixShip != mixShip1;
         }
